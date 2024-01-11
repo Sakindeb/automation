458410fa8cc378b9470b4ecef8f071d43e4429f2
Sakindeb <sakindeborah@gmail.com>
2024-01-11 10:39:57 +0300

    Making calls to the API

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient;
+import java.io.IOException;
 
-public class Main {
+class call {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes"))
+                .header("X-RapidAPI-Host", "jokes-by-api-ninjas.p.rapidapi.com")
+                .header("X-RapidAPI-Key", "9cd2d39a39msh787fda8ae99b978p1a705djsn184d0ca81614")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(response.body());
     }
 }
